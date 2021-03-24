f9bcafa7457d1f53850fda0d6689b028c51acfaa
Gopal Reekoye <rkygop001@nightmare.cs.uct.ac.za>
2021-03-24 14:26:27 +0200

    added header file for methods

---
+++ methods.h
@@ -0,0 +1,41 @@
+//Reekoye Gopal
+//24/03/2021
+//
+//
+//methods.h
+//
+//
+
+
+#ifndef _methods_h
+#define _methods_h
+
+
+//function declarations
+
+//to read and parse a specified input file
+
+readfile(string txt);
+
+//to print all tags
+
+string print();
+
+//dump/write tags and data to  file called tag.txt
+
+dump();
+
+//list/print tag data for a given tag
+
+string dump(TagStruct tagstruct);
+
+//quit-terminate the input loop and exit
+
+quit();
+
+
+#endif //matches the # ifndef
+
+
+
+
